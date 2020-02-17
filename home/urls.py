86d426b147839c254a7a3b414a475093970d7174
arjanlangit <heaven.arjan@gmail.com>
2020-02-17 21:21:14 +0800

    Update and Delete Student

--- home/urls.py
+++ home/urls.py
@@ -9,8 +9,9 @@ urlpatterns = [
     path('sections/create/', views.SectionCreateView.as_view(), name='create-section'),
 
     # NOTE temp
-    path('student/view/', views.StudentIndividualView.as_view(), name='view-student'),
     path('student/<int:pk>/', views.StudentsListView.as_view(), name='student-list-view'),
+    path('student/view/<int:pk>/', views.StudentDetailView.as_view(), name='view-student'),
     path('student/create/<int:pk>/', views.StudentCreateView.as_view(), name='create-student'),
     path('student/update/<int:pk>/', views.StudentUpdateView.as_view(), name='update-student'),
+    path('student/delete/<int:pk>/', views.delete_student, name='delete-student'),
 ]
